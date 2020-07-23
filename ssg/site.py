b81af91536ff228dad48e827871f176ca2d9b4f7
Brent Johnson <33670213+BrentTJohnson@users.noreply.github.com>
2020-07-23 13:24:15 -0400

    Module 2 completed
    Set up the Site class and CLI

---
+++ ssg/site.py
@@ -0,0 +1,17 @@
+import Path from pathlib
+
+
+class Site():
+    def __init__(self, source, dest):
+        self.source = Path()
+        self.dest   = Path()
+
+    def create_dir(self, path):
+        directory = self.dest + '/' + relative_to(self.source)
+        mkdir(directory, parents=True, exist_ok=True)
+
+    def build():
+        mkdir(self.dest, parets=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if isdir(path):
+                create_dir(path) 
