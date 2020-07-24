6b3d2c3c30447653f6e41cf39dc186eb88612e97
Brent Johnson <33670213+BrentTJohnson@users.noreply.github.com>
2020-07-24 12:58:28 -0400

    Corrections to module 2
    corrected syntax in Parser and Site packages

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -13,19 +13,19 @@ class Parser():
         return extension in self.extensions
 
     def parse(self, path: Path, source: Path, dest: Path):
-        raise NotImplimentedError
+        raise NotImplementedError
 
     def read(self, path):
         with open(path, 'r') as file:
             return file.read()
 
-    def write(self, path, dest, content, ext='.xml'):
-        full_path = self.dest / path.with_suffix(ext).name
+    def write(self, path, dest, content, ext='.html'):
+        full_path = dest / path.with_suffix(ext).name
         with open(full_path, 'w') as file:
             file.write(content)
 
     def copy(self, path, source , dest):
-        shutil.copy2(path, dest / path.relativeto(source))
+        shutil.copy2(path, dest / path.relative_to(source))
 
 
 class ResourceParser(Parser):
